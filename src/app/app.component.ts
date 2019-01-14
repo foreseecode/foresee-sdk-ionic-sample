3f90957717affe1a74d82c405874948ae20e50c1
Joshua Strickland <joshua.strickland@foresee.com>
2019-01-14 11:43:09 -0500

    ForeSee Cordova plugin implemented in project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,8 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
+
+declare var cordova;
 @Component({
   templateUrl: 'app.html'
 })
@@ -12,6 +14,9 @@ export class MyApp {
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
+      cordova.plugins.ForeSeeAPI.setDebugLogEnabled([true], function success(data) {
+        console.log("debug enabled success: " + data)
+      });
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+declare var cordova;
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
@@ -9,6 +10,17 @@ export class HomePage {
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+  onSuccess(data) {
+    console.log("Succcess. Data: " + data);
+  }
+  onFailure(data) {
+    console.log("Failure. Data: " + data);
+  }
+
+  showInvite() {
+    cordova.plugins.ForeSeeAPI.showInvite(['app_test_1'], this.onSuccess, this.onFailure);
   }
 
 }
